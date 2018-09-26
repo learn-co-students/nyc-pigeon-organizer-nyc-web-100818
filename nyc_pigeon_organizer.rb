83cd6c2fe73f45a83a4cee6ef6f9c8e2bf6fa8c3
Chett Tiller <chett.tiller@gmail.com>
2018-09-26 16:30:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
+def pigeon_namer(data)
+  pigeon_names = []
+  data.each do |k,v|
+    v.values.each do |names|
+      names.each do |name|
+        pigeon_names << name unless pigeon_names.include?(name)
+      end
+    end
+  end
+  pigeon_names
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names = pigeon_namer(data)
+  pigeon_list = {}
+  pigeon_names.each do |pigeon|
+    pigeon_list.merge!(pigeon => {})
+    data.each do |key,value|
+      pigeon_list[pigeon].merge!(key => [])
+      value.each do |k,v|
+        pigeon_list[pigeon][key].push(k.to_s) if v.include?(pigeon)
+      end
+    end
+  end
+  pigeon_list
 end
