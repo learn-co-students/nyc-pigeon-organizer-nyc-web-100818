528425a27725961dc97e4f0d4c56fb8244726d37
Cara Morelli <caramorelli7@gmail.com>
2018-09-29 23:37:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_by_name = {}
+  name_arr = data[:gender][:male] += data[:gender][:female] #["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky", "Queenie", "Ms. K"]
+  name_arr.each do |name|
+    pigeon_by_name[name] = { :color => [], :gender => [], :lives => []}
+  end 
+  data.each do |key, val|
+    val.each do |k, v|
+      v.each do |names|
+        if k.to_s == 'female'
+          pigeon_by_name[names][key] = [k.to_s] 
+        else 
+          pigeon_by_name[names][key] << k.to_s 
+        end 
+      end 
+    end 
+  end 
+  pigeon_by_name
 end
